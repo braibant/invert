009051e7f2f551c2a39449749a205d2073643844
Pierre Boutillier <pierre.boutillier@inria.fr>
2013-03-22 14:27:07 +0100

    Vector tests

--- test.v
+++ test.v
@@ -1,40 +1,54 @@
 Inductive even : nat -> Type :=
-  | even_0 : even 0 
+  | even_0 : even 0
   | even_SS : forall n, even n -> even (S (S n)).
 
-Declare ML Module "invert". 
-Lemma l1 : forall n, even (2 + n) -> even n. 
-  intros. 
-  set (diag := fun x =>                                
-                 match x return Type with
-                   | S (S x1) => even x1 
-                   | _ => even x
-                 end). 
-  
-  refine (match H in even args return diag args
-           with 
-             | even_0 => _
-             | even_SS n x => _
-           end). simpl.  constructor. 
-  simpl. auto. 
-  Restart. 
-  intros. 
-  set (diag := fun x =>                                
-                 match x return Type with
-                   | S (S x1) => even x1 
-                   | _ => even n
-                 end). 
-  refine (match H in even args return diag args
-          with 
-            | even_0 => _
-            | even_SS n x => _
-          end); simpl.  
-  Restart.
-  intros. 
-  invert H. simpl. auto. simpl. auto.
+Declare ML Module "invert".
+
+Lemma l1 : forall n, even (2 + n) -> even n.
+Proof.
+  intros.
+  invert H. simpl.  auto. simpl. auto.
  Show Proof. 
 Qed. 
 
+Section ex1. 
+  Variable A : Type.
+  
+  Inductive vector: nat ->  Type :=
+    |   nil : vector 0
+    | cons : forall n : nat, forall x : A, forall v : vector n, vector (S n).
+
+  Inductive P : forall n:nat, vector n -> Prop :=
+    | Pnil : P 0 nil
+    | Pcons : forall n v, P n v -> forall h, P (S n) (cons n h v). 
+    
+  Lemma foo n h v: 
+    P (S n) (cons n h v) -> P n v.
+      intros H.
+        invert H.  auto. auto. Show Proof. 
+  Qed.
+End ex1.  
+
+Module vectors. 
+ 
+Inductive t A : nat -> Type :=
+  |nil : t A 0
+  |cons : forall (h:A) (n:nat), t A n -> t A (S n).
+
+Local Notation "[]" := (nil _).
+Local Notation "h :: t" := (cons _ h _ t) (at level 60, right associativity).
+
+Section SCHEMES.
+
+(** An induction scheme for non-empty vectors *)
+
+Lemma rectS {A} (P:forall {n}, t A (S n) -> Type)
+ (bas: forall a: A, P (a :: []))
+ (rect: forall a {n} (v: t A (S n)), P v -> P (a :: v)) :  
+forall n (v: t A (S n)),  P v.
+fix 2. intros n v. invert v.  auto. intros. destruct n0. invert X. apply  bas. auto.
+apply rect. apply rectS.
+Qed.
 
 Inductive mul3 : nat -> Prop :=
   | T0 : mul3 0
